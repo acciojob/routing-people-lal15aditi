9655eab8f1b9cc9a90b0672458afe1e6e9ffd255
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-03-21 19:24:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
-import React from "react";
-import './../styles/App.css';
-
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import users from "./users";
+//
+import "./../styles/App.css";
+import UserList from "./UserList";
+import UserDetails from "./UserDetails";
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        {
+          users.map((user, index) => (
+            <Route key={index} path={`/users/${user.id}`} element={<UserDetails userId={user.id} />} />
+          ))
+        }
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
