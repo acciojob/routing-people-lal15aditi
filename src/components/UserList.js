b264f4a9ac9a54de1f06105a4b884d81f8832c92
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-03-21 19:25:40 +0530

    Create UserList.js

---
+++ src/components/UserList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import users from "./users";
+import { Link } from "react-router-dom/dist";
+function UserList() {
+  return (
+    <div className="list">
+      <h1>User List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/users/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserList;
